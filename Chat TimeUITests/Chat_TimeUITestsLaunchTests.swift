36e5e2a650bea47d6544d7b454b86cd636de7af6
BdriahFakhrji <bdriahtalaat@gmail.com>
2023-01-08 22:24:39 +0300

    Initial Commit

---
+++ Chat TimeUITests/Chat_TimeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chat_TimeUITestsLaunchTests.swift
+//  Chat TimeUITests
+//
+//  Created by Bdriah Talaat on 15/06/1444 AH.
+//
+
+import XCTest
+
+final class Chat_TimeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
